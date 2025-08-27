f423d563f78fa1727efa1b254cc06475f5f838c9
Aryan <aryan.nepali80@gmail.com>
2025-08-27 13:43:06 +0000

    added current locationon the map

--- lib/widgets/map_picker.dart
+++ lib/widgets/map_picker.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapPicker extends StatefulWidget {
   final Function(LatLng) onLocationSelected;
@@ -12,7 +13,29 @@ class MapPicker extends StatefulWidget {
 }
 
 class _MapPickerState extends State<MapPicker> {
+  final MapController _mapController = MapController();
   LatLng _selectedLocation = LatLng(27.7172, 85.3240); // Default: Kathmandu
+  LatLng? _currentLocation;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    final permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied || permission == LocationPermission.deniedForever) {
+      return;
+    }
+
+    final position = await Geolocator.getCurrentPosition();
+    final location = LatLng(position.latitude, position.longitude);
+    setState(() {
+      _currentLocation = location;
+      _mapController.move(location, 15.0);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +46,7 @@ class _MapPickerState extends State<MapPicker> {
         SizedBox(
           height: 300,
           child: FlutterMap(
-            mapController: MapController(),
+            mapController: _mapController,
             options: MapOptions(
               initialCenter: _selectedLocation,
               initialZoom: 13.0,
@@ -45,6 +68,13 @@ class _MapPickerState extends State<MapPicker> {
                     height: 40,
                     child: const Icon(Icons.location_pin, color: Colors.red, size: 40),
                   ),
+                  if (_currentLocation != null)
+                    Marker(
+                      point: _currentLocation!,
+                      width: 30,
+                      height: 30,
+                      child: const Icon(Icons.my_location, color: Colors.blue, size: 30),
+                    ),
                 ],
               ),
             ],
